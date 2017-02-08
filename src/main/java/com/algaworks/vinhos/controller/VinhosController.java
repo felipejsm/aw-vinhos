c4c7480505526e859217b060937620288fa3b997
felipejsm <felipejsm41@gmail.com>
2017-02-08 21:26:28 -0200

    Post

--- src/main/java/com/algaworks/vinhos/controller/VinhosController.java
+++ src/main/java/com/algaworks/vinhos/controller/VinhosController.java
@@ -2,11 +2,18 @@ package com.algaworks.vinhos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.algaworks.vinhos.model.Vinho;
 
 @Controller
 public class VinhosController {
 	@GetMapping("/vinhos/novo")
 	public String novo() {
 		return "vinho/cadastro-vinho";
+	}
+	@PostMapping("/vinhos/novo")
+	public String salvar(Vinho vinho) {
+		return "";
 	}
 }
