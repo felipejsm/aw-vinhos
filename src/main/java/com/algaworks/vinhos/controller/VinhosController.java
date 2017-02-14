3cc74810a27b8fb2b35f1ecc253e9b8306bbe600
felipejsm <felipejsm41@gmail.com>
2017-02-13 22:25:57 -0200

    Add metodo de busca

--- src/main/java/com/algaworks/vinhos/controller/VinhosController.java
+++ src/main/java/com/algaworks/vinhos/controller/VinhosController.java
@@ -1,6 +1,7 @@
 package com.algaworks.vinhos.controller;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,27 +10,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.vinhos.model.TipoVinho;
 import com.algaworks.vinhos.model.Vinho;
 import com.algaworks.vinhos.repository.Vinhos;
+import com.algaworks.vinhos.repository.filter.VinhoFilter;
 
 @Controller
+@RequestMapping("/vinhos")
 public class VinhosController {
 	
 	@Autowired
 	private Vinhos vinhos;
 	
-	@GetMapping("/vinhos/novo")
+	@GetMapping("/novo")
 	public ModelAndView novo(Vinho vinho) {
 		ModelAndView mv = new ModelAndView("vinho/cadastro-vinho");
 		mv.addObject("tipos", TipoVinho.values());
 		return mv;
 	}
 	//@Valid -> se houver error, atribue o valor ao result e valida depois(se deu erro ou não)
-	@PostMapping("/vinhos/novo")
+	@PostMapping("/novo")
 	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result,RedirectAttributes attributes) {
 		if(vinho.getValor() == null) {
 			vinho.setValor(new BigDecimal(4.9));
@@ -40,5 +44,12 @@ public class VinhosController {
 		//permite enviar algum atributo no redirect na página, normalmente de sucesso
 		attributes.addFlashAttribute("mensagem", "Vinho salvo com sucesso!");
 		return new ModelAndView("redirect:/vinhos/novo");
+	}
+	@GetMapping
+	public ModelAndView pesquisar(VinhoFilter vinhoFilter) {
+		ModelAndView mv = new ModelAndView("/vinho/pesquisa-vinhos");
+		mv.addObject("vinhos", vinhos.findByNomeContainingIgnoreCase(
+				Optional.ofNullable(vinhoFilter.getNome()).orElse("%")));
+		return mv;
 	}
 }
